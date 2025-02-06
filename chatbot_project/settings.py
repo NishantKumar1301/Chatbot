d269c0027ac8139cabfcc5c76f8a3f6ecf93924a
Nishant Kumar <nishantkumarfps@gmail.com>
2025-02-07 04:08:30 +0530

    Updated the settings.py

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8mllbtr)nsq2&t9^b*z1^fhhvhn#p_r^34xxpa(30_c5h9ei1v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
